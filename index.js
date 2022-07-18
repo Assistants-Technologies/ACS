fd8cc9f81d52879d6db7ced131e44a79cdb7983d
Breathtake <breftejk@gmail.com>
2022-07-18 17:01:46 +0200

    https

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 require('dotenv').config({})
+const DEVELOPMENT_CHANNEL = process.env.DEVELOPMENT_CHANNEL === "TRUE";
+
+console.log(DEVELOPMENT_CHANNEL)
 
 const express = require('express')
 const https = require('https')
@@ -9,12 +12,12 @@ app.get('*', (req,res)=>{
     res.status(200).send({error:false,message:"Oracle ACS VPS test."})
 })
 
-if(process.env.DEVELOPMENT_CHANNEL){
+if(DEVELOPMENT_CHANNEL){
     app.listen(80)
 }else{
     const server = https.createServer(app, {
         requestCert: false,
-        rejectUnauthorized: false,
+        rejectUnauthorized: true,
         key: fs.readFileSync('/etc/cert/assistantscenter.com/privkey.pem'),
         cert: fs.readFileSync('/etc/cert/assistantscenter.com/fullchain.pem')
     })
