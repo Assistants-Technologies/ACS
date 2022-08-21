df2593cbc00f14088435ad969991c2bdb97dabb5
Marcin Kondrat <breftejk@gmail.com>
2022-08-21 16:40:07 +0200

    Update runner.js

--- projects/main/api/discord-dashboard/plan/dbd_subscriptions/runner.js
+++ projects/main/api/discord-dashboard/plan/dbd_subscriptions/runner.js
@@ -17,10 +17,8 @@ module.exports = ({client}) => {
 
         const All_Subscriptions = await DiscordDashboard.find({})
         for(const Subscription of All_Subscriptions){
-            if(Subscription.plan?.plan_type == 'premium'/* && (Date.now() > Subscription.plan?.active_until)*/){
+            if(Subscription.plan?.plan_type == 'premium' && (Date.now() > Subscription.plan?.active_until)){
                 const subscription = await stripe.subscriptions.retrieve(Subscription.plan.subscription.id)
-                const latest_invoice = await stripe.invoices.retrieve('in_1LYsIHKWxgCmg6SfIEUiJ0aR')
-                console.log(latest_invoice)
                 if(subscription.status != "active"){
                     try{
                         await stripe.subscriptions.cancel(Subscription.plan.subscription.id)
@@ -49,28 +47,59 @@ module.exports = ({client}) => {
                         Disable 9 of 10 DBD Projects
                     */
                 }else{
-                    const Plan_To_Update = await DiscordDashboard.findOne({
-                        _id: Subscription._id,
-                    })
-
-                    Plan_To_Update.plan = {
-                        plan_type: 'premium',
-                        subscription,
-                        active: true,
-                        active_until: new Date(subscription.current_period_end*1000),
-                        session: Plan_To_Update.session,
-                    }
-
-                    await Plan_To_Update.save()
-
                     try{
-                        client.guilds.cache.get(process.env.DISCORD_GUILD_ID).channels.cache.get(process.env.DISCORD_LOGS_CHANNEL_ID).send(`Renew plan for ${Plan_To_Update.user}`)
+                        client.guilds.cache.get(process.env.DISCORD_GUILD_ID).channels.cache.get(process.env.DISCORD_LOGS_CHANNEL_ID).send(`Subscription expired for ${Plan_To_Update.user}? (checking for latest invoice)`)
                     }catch(err){
                     }
+                    const latest_invoice = await stripe.invoices.retrieve(Subscription.plan.subscription.latest_invoice)
 
-                    /**
-                     * Send invoice
-                     */
+                    if(latest_invoice.status == 'uncollectible'){
+                        try{
+                            await stripe.subscriptions.cancel(Subscription.plan.subscription.id)
+                        }catch(err){}
+    
+                        const Plan_To_Downgrade = await DiscordDashboard.findOne({
+                            _id: Subscription._id,
+                        })
+    
+                        Plan_To_Downgrade.plan = {
+                            plan_type: 'free',
+                            subscription: null,
+                            active: false,
+                            active_until: null,
+                            session: null,
+                        }
+    
+                        await Plan_To_Downgrade.save()
+    
+                        try{
+                            client.guilds.cache.get(process.env.DISCORD_GUILD_ID).channels.cache.get(process.env.DISCORD_LOGS_CHANNEL_ID).send(`Downgrade plan for ${Plan_To_Downgrade.user} (invoice not paid on renew)`)
+                        }catch(err){}
+                    }else if(latest_invoice.status == 'paid'){
+                        const Plan_To_Update = await DiscordDashboard.findOne({
+                            _id: Subscription._id,
+                        })
+    
+                        Plan_To_Update.plan = {
+                            plan_type: 'premium',
+                            subscription,
+                            active: true,
+                            active_until: new Date(subscription.current_period_end*1000),
+                            session: Plan_To_Update.session,
+                        }
+    
+                        await Plan_To_Update.save()
+    
+                        try{
+                            client.guilds.cache.get(process.env.DISCORD_GUILD_ID).channels.cache.get(process.env.DISCORD_LOGS_CHANNEL_ID).send(`Renew plan for ${Plan_To_Update.user} (invoice paid, so real renew)`)
+                        }catch(err){
+                        }
+                    }else{
+                        try{
+                            client.guilds.cache.get(process.env.DISCORD_GUILD_ID).channels.cache.get(process.env.DISCORD_LOGS_CHANNEL_ID).send(`Renew plan for ${Plan_To_Update.user} (still waiting)`)
+                        }catch(err){
+                        }
+                    }
                 }
             }
         }
