79a8b647d8e621f2cc028b2dbf2b6c7c5e7f155b
Marcin Kondrat <breftejk@gmail.com>
2022-08-23 20:36:24 +0200

    Create route.js

---
+++ projects/main/api/partnership/route.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+
+const User = require('../../../../models/user')
+const DiscordDashboard = require('../../../../models/discordDashboard')
+
+router.get('/', async (req,res)=>{
+})
+
+module.exports = router
