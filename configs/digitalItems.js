c2fcc66ab9a7955f5fc55cdd37012c371235bda5
iMidnight <mohaidarus@gmail.com>
2022-12-28 19:54:41 +0000

    Added license regen

--- configs/digitalItems.js
+++ configs/digitalItems.js
@@ -60,17 +60,18 @@ module.exports = {
                 },
             ]
         },*/
-            {
-                categoryName: "Discord-Dashboard v2 Licenses",
-                categoryDescription: "Choose a license you'd like to use.",
-                categoryItems: [
-                    {
-                        id: "dbd_opensource_license",
-                        image: "https://cdn.assistantscenter.com/l6yvtyo6",
-                        aho: {
-                            type: 'modal',
-                            html: {
-                                content: `
+        {
+            categoryId: "dbd-license",
+            categoryName: "Discord-Dashboard v2 Licenses",
+            categoryDescription: "Choose a license you'd like to use.",
+            categoryItems: [
+                {
+                    id: "dbd_opensource_license",
+                    image: "https://cdn.assistantscenter.com/l6yvtyo6",
+                    aho: {
+                        type: 'modal',
+                        html: {
+                            content: `
                                 <div className="modal-header">
                                     <h3 className="modal-title" id="exampleModalLabel">OpenSource DBD v2 License</h3>
                                 </div>
@@ -82,53 +83,53 @@ module.exports = {
                                     <a id="license"></a>
                                 </div>
                             `,
-                            },
-                            getInfo: async ({ user_id }) => {
-                                const user = await User.findOne({ _id: user_id })
-                                return user?.OpenSource?.license_id
-                            }
                         },
-                       /*aho: {
-                            type: 'redirect',
-                            url: '/discord-dashboard/v2'
-                        },*/
-                        name: "OpenSource DBD v2 License",
-                        description: "This license is for the OpenSource DBD v2 project. It is free to use and can be used by anyone.",
-                        price: 0,
-                        owns_already: async ({ user_id }) => {
+                        getInfo: async ({ user_id }) => {
                             const user = await User.findOne({ _id: user_id })
-                            return user?.OpenSource?.license_id == null ? false : true
-                        },
-                        assign_item: async ({ Session }) => {
-                            const user_id = Session.user._id
-                            const user = await User.findOne({ _id: user_id })
-
-                            const license = uuidv4()
-                            user.OpenSource = {
-                                payment_id: 'UNKNOWN',
-                                license_type: 'opensource',
-                                status: 'approved',
-                                license_id: license,
-                            }
-                            await user.save()
-
-                            await LicensesList.create({
-                                user: user_id,
-                                payment_id: 'UNKNOWN',
-                                license_id: license,
-                                license_type: 'opensource',
-                            })
-
-                            return true
-                        },
+                            return user?.OpenSource?.license_id
+                        }
                     },
-                    {
-                        id: "dbd_personal_license",
-                        image: "https://cdn.assistantscenter.com/l6yvtyo6",
-                        aho: {
-                            type: 'modal',
-                            html: {
-                                content: `
+                    /*aho: {
+                         type: 'redirect',
+                         url: '/discord-dashboard/v2'
+                     },*/
+                    name: "OpenSource DBD v2 License",
+                    description: "This license is for the OpenSource DBD v2 project. It is free to use and can be used by anyone.",
+                    price: 0,
+                    owns_already: async ({ user_id }) => {
+                        const user = await User.findOne({ _id: user_id })
+                        return user?.OpenSource?.license_id == null ? false : true
+                    },
+                    assign_item: async ({ Session }) => {
+                        const user_id = Session.user._id
+                        const user = await User.findOne({ _id: user_id })
+
+                        const license = uuidv4()
+                        user.OpenSource = {
+                            payment_id: 'UNKNOWN',
+                            license_type: 'opensource',
+                            status: 'approved',
+                            license_id: license,
+                        }
+                        await user.save()
+
+                        await LicensesList.create({
+                            user: user_id,
+                            payment_id: 'UNKNOWN',
+                            license_id: license,
+                            license_type: 'opensource',
+                        })
+
+                        return true
+                    },
+                },
+                {
+                    id: "dbd_personal_license",
+                    image: "https://cdn.assistantscenter.com/l6yvtyo6",
+                    aho: {
+                        type: 'modal',
+                        html: {
+                            content: `
                                 <div className="modal-header">
                                     <h3 className="modal-title" id="exampleModalLabel">Personal DBD v2 License</h3>
                                 </div>
@@ -140,53 +141,53 @@ module.exports = {
                                     <a id="license"></a>
                                 </div>
                             `,
-                            },
-                            getInfo: async ({ user_id }) => {
-                                const user = await User.findOne({ _id: user_id })
-                                return user?.Personal?.license_id
-                            }
                         },
-                        /*aho: {
-                            type: 'redirect',
-                            url: '/discord-dashboard/v2'
-                        },*/
-                        name: "Personal DBD v2 License",
-                        description: "This license is for the Personal DBD v2 project. It allows profit-making from the project.",
-                        price: 1000,
-                        owns_already: async ({ user_id }) => {
+                        getInfo: async ({ user_id }) => {
                             const user = await User.findOne({ _id: user_id })
-                            return user?.Personal?.license_id == null ? false : true
-                        },
-                        assign_item: async ({ Session }) => {
-                            const user_id = Session.user._id
-                            const user = await User.findOne({ _id: user_id })
-
-                            const license = uuidv4()
-                            user.Personal = {
-                                payment_id: 'UNKNOWN',
-                                license_type: 'personal',
-                                status: 'approved',
-                                license_id: license,
-                            }
-                            await user.save()
-
-                            await LicensesList.create({
-                                user: user_id,
-                                payment_id: 'UNKNOWN',
-                                license_id: license,
-                                license_type: 'personal',
-                            })
-
-                            return true
+                            return user?.Personal?.license_id
                         }
                     },
-                    {
-                        id: "dbd_production_license",
-                        image: "https://cdn.assistantscenter.com/l6yvtyo6",
-                        aho: {
-                            type: 'modal',
-                            html: {
-                                content: `
+                    /*aho: {
+                        type: 'redirect',
+                        url: '/discord-dashboard/v2'
+                    },*/
+                    name: "Personal DBD v2 License",
+                    description: "This license is for the Personal DBD v2 project. It allows profit-making from the project.",
+                    price: 1000,
+                    owns_already: async ({ user_id }) => {
+                        const user = await User.findOne({ _id: user_id })
+                        return user?.Personal?.license_id == null ? false : true
+                    },
+                    assign_item: async ({ Session }) => {
+                        const user_id = Session.user._id
+                        const user = await User.findOne({ _id: user_id })
+
+                        const license = uuidv4()
+                        user.Personal = {
+                            payment_id: 'UNKNOWN',
+                            license_type: 'personal',
+                            status: 'approved',
+                            license_id: license,
+                        }
+                        await user.save()
+
+                        await LicensesList.create({
+                            user: user_id,
+                            payment_id: 'UNKNOWN',
+                            license_id: license,
+                            license_type: 'personal',
+                        })
+
+                        return true
+                    }
+                },
+                {
+                    id: "dbd_production_license",
+                    image: "https://cdn.assistantscenter.com/l6yvtyo6",
+                    aho: {
+                        type: 'modal',
+                        html: {
+                            content: `
                                 <div className="modal-header">
                                     <h3 className="modal-title" id="exampleModalLabel">Production DBD v2 License</h3>
                                 </div>
@@ -198,47 +199,47 @@ module.exports = {
                                     <a id="license"></a>
                                 </div>
                             `,
-                            },
-                            getInfo: async ({ user_id }) => {
-                                const user = await User.findOne({ _id: user_id })
-                                return user?.Production?.license_id
-                            }
                         },
-                        /*aho: {
-                            type: 'redirect',
-                            url: '/discord-dashboard/v2'
-                        },*/
-                        name: "Production DBD v2 License",
-                        description: "This license is for the Personal DBD v2 project. It allows profit-making from the project. It can be shared with friends.",
-                        price: 10000,
-                        owns_already: async ({ user_id }) => {
+                        getInfo: async ({ user_id }) => {
                             const user = await User.findOne({ _id: user_id })
-                            return user?.Production?.license_id == null ? false : true
-                        },
-                        assign_item: async ({ Session }) => {
-                            const user_id = Session.user._id
-                            const user = await User.findOne({ _id: user_id })
-
-                            const license = uuidv4()
-                            user.Production = {
-                                payment_id: 'UNKNOWN',
-                                license_type: 'production',
-                                status: 'approved',
-                                license_id: license,
-                            }
-                            await user.save()
-
-                            await LicensesList.create({
-                                user: user_id,
-                                payment_id: 'UNKNOWN',
-                                license_id: license,
-                                license_type: 'production',
-                            })
-
-                            return true
+                            return user?.Production?.license_id
                         }
+                    },
+                    /*aho: {
+                        type: 'redirect',
+                        url: '/discord-dashboard/v2'
+                    },*/
+                    name: "Production DBD v2 License",
+                    description: "This license is for the Personal DBD v2 project. It allows profit-making from the project. It can be shared with friends.",
+                    price: 10000,
+                    owns_already: async ({ user_id }) => {
+                        const user = await User.findOne({ _id: user_id })
+                        return user?.Production?.license_id == null ? false : true
+                    },
+                    assign_item: async ({ Session }) => {
+                        const user_id = Session.user._id
+                        const user = await User.findOne({ _id: user_id })
+
+                        const license = uuidv4()
+                        user.Production = {
+                            payment_id: 'UNKNOWN',
+                            license_type: 'production',
+                            status: 'approved',
+                            license_id: license,
+                        }
+                        await user.save()
+
+                        await LicensesList.create({
+                            user: user_id,
+                            payment_id: 'UNKNOWN',
+                            license_id: license,
+                            license_type: 'production',
+                        })
+
+                        return true
                     }
-                ]
-            }
-        ]
+                }
+            ]
+        }
+    ]
 }
--- src/pages/discord-dashboard-v2.js
+++ src/pages/discord-dashboard-v2.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Head from 'next/head'
 
+import axios from 'axios'
+
 import Script from 'next/script'
 import PageBody from "../components/content/PageBody"
 import Scripts from "../components/content/Scripts"
@@ -14,22 +16,41 @@ export async function getServerSideProps(context) {
     return {
         props: {
             url: context.query.url.split('?')[0],
-            licenses: context.query.licenses,
+            license: context.query.licenses,
             user: context.query.user,
         },
     }
 }
 
-export default function TestPage ({ user, url, licenses }) {
+export default function TestPage({ user, url, license }) {
     const ud = (url.split("/").length - 1)
     let ud_s = ''
-    if(ud != 1){
-        for(let i = 0; i < ud; i++){
+    if (ud != 1) {
+        for (let i = 0; i < ud; i++) {
             ud_s += '../'
         }
     }
 
+    const [licenses, setLicenses] = React.useState(license)
     const title = `${IsBeta ? 'BETA | ' : ''}Assistants Center - Discord Dashboard v2`
+
+    const refreshLicense = async (type) => {
+        const response = await axios.post(`/api/discord-dashboard/license/regen/dbd?type=${type}`)
+
+        if (response.data?.error === false) {
+            type = type.toLowerCase()
+
+            let newLicenses = {}
+
+            Object.keys(licenses).forEach((key) => {
+                if (key.toLowerCase() === type) {
+                    newLicenses[key] = response.data.newLicense
+                } else newLicenses[key] = licenses[key]
+            })
+
+            setLicenses(newLicenses)
+        }
+    }
 
     return (
         <>
@@ -42,7 +63,7 @@ export default function TestPage ({ user, url, licenses }) {
                 <script type="text/javascript" dangerouslySetInnerHTML={{ __html: `!function(){var e=document.createElement("script");e.type="text/javascript",e.src="https://global.ketchcdn.com/web/v2/config/assistantscenter/website_smart_tag/boot.js",e.defer=e.async=!0,document.getElementsByTagName("head")[0].appendChild(e),window.semaphore=window.semaphore||[]}();` }}></script>
                 <title>{title}</title>
                 <link rel="stylesheet" href={`${ud_s}vendors/feather/feather.css`} />
-                <link rel="stylesheet" href={`${ud_s}vendors/mdi/css/materialdesignicons.min.css`}/>
+                <link rel="stylesheet" href={`${ud_s}vendors/mdi/css/materialdesignicons.min.css`} />
                 <link rel="stylesheet" href={`${ud_s}vendors/ti-icons/css/themify-icons.css`} />
                 <link rel="stylesheet" href={`${ud_s}vendors/typicons/typicons.css`} />
                 <link
@@ -59,11 +80,11 @@ export default function TestPage ({ user, url, licenses }) {
                 {
                     IsBeta ?
                         <link rel="shortcut icon" href={`${ud_s}images/favicon.png`} />
-                    :
+                        :
                         <link rel="shortcut icon" href={`${ud_s}favicon.png`} />
                 }
 
-                <link  rel="stylesheet" href={`${ud_s}mycss/my_dark_css.css`}/>
+                <link rel="stylesheet" href={`${ud_s}mycss/my_dark_css.css`} />
             </Head>
             <PageBody user={user} uds={ud_s}>
                 <div className="row">
@@ -94,28 +115,28 @@ export default function TestPage ({ user, url, licenses }) {
                                                 <div className="table-responsive">
                                                     <table className="table table-striped">
                                                         <thead>
-                                                        <tr>
-                                                            <th width="25%">Type</th>
-                                                            <th width="75%">License ID</th>
-                                                        </tr>
+                                                            <tr>
+                                                                <th width="25%">Type</th>
+                                                                <th width="75%">License ID</th>
+                                                            </tr>
                                                         </thead>
                                                         <tbody>
-                                                        <tr>
-                                                            <td><b>OpenSource</b></td>
-                                                            <td>{licenses.OpenSource ?? <a href="/shop">Generate now</a>}</td>
-                                                        </tr>
-                                                        <tr>
-                                                            <td><b>Personal</b></td>
-                                                            <td>{licenses.Personal ?? <a href="/shop">Buy now</a>}</td>
-                                                        </tr>
-                                                        <tr>
-                                                            <td><b>Production</b></td>
-                                                            <td>{licenses.Production ?? <a href="/shop">Buy now</a>}</td>
-                                                        </tr>
+                                                            <tr>
+                                                                <td><b>OpenSource</b></td>
+                                                                <td>{licenses.OpenSource ? <>{licenses.OpenSource} <button onClick={() => refreshLicense("opensource")} className='btn btn-primary' style={{ margin: "auto 10px" }}><i className='mdi mdi-refresh' style={{ margin: "auto", display: "flex", justifyContent: "center" }}></i></button></> : <a href="/shop">Generate now</a>}</td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td><b>Personal</b></td>
+                                                                <td>{licenses.Personal ? <>{licenses.Personal} <button onClick={() => refreshLicense("personal")} className='btn btn-primary' style={{ margin: "auto 10px" }}><i className='mdi mdi-refresh' style={{ margin: "auto", display: "flex", justifyContent: "center" }}></i></button></> : <a href="/shop">Generate now</a>}</td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td><b>Production</b></td>
+                                                                <td>{licenses.Production ? <>{licenses.Production} <button onClick={() => refreshLicense("production")} className='btn btn-primary' style={{ margin: "auto 10px" }}><i className='mdi mdi-refresh' style={{ margin: "auto", display: "flex", justifyContent: "center" }}></i></button></> : <a href="/shop">Generate now</a>}</td>
+                                                            </tr>
                                                         </tbody>
                                                     </table>
                                                 </div>
-                                                <div style={{paddingTop:'30px'}}>
+                                                <div style={{ paddingTop: '30px' }}>
                                                     {
                                                         (!licenses.OpenSource || !licenses.Personal || !licenses.Production) ?
                                                             <p>
@@ -137,14 +158,14 @@ export default function TestPage ({ user, url, licenses }) {
                     </div>
                 </div>
             </PageBody>
-            <Scripts src={ud_s}/>
-            <Script src={`${ud_s}vendors/js/vendor.bundle.base.js`}/>
-            <Script src={`${ud_s}vendors/bootstrap-datepicker/bootstrap-datepicker.min.js`}/>
-            <Script src={`${ud_s}js/off-canvas.js`}/>
-            <Script src={`${ud_s}js/hoverable-collapse.js`}/>
-            <Script src={`${ud_s}js/template.js`}/>
-            <Script src={`${ud_s}js/settings.js`}/>
-            <Script src={`${ud_s}js/todolist.js`}/>
+            <Scripts src={ud_s} />
+            <Script src={`${ud_s}vendors/js/vendor.bundle.base.js`} />
+            <Script src={`${ud_s}vendors/bootstrap-datepicker/bootstrap-datepicker.min.js`} />
+            <Script src={`${ud_s}js/off-canvas.js`} />
+            <Script src={`${ud_s}js/hoverable-collapse.js`} />
+            <Script src={`${ud_s}js/template.js`} />
+            <Script src={`${ud_s}js/settings.js`} />
+            <Script src={`${ud_s}js/todolist.js`} />
         </>
     )
 }
