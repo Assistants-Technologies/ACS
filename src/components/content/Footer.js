9cd7ab3293f43869969c6d7a7a52c9c7cbb9e995
Marcin Kondrat <breftejk@gmail.com>
2023-02-13 19:38:48 +0100

    21-23

--- src/components/content/Footer.js
+++ src/components/content/Footer.js
@@ -6,7 +6,7 @@ export default function Footer (props) {
             <div className="d-sm-flex justify-content-center justify-content-sm-between">
                 <span className="text-muted text-center text-sm-left d-block d-sm-inline-block">
                     Template by <a href="https://www.bootstrapdash.com/" target="_blank">BootstrapDash</a>.</span>
-                <span className="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Copyright © 2023 Assistants Center. All rights reserved.</span>
+                <span className="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Copyright © 2021-2023 Assistants Center. All rights reserved.</span>
             </div>
         </footer>
     )
